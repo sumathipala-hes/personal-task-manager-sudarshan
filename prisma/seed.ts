f71f382b83f159789d111be1254620d1a6d3edba
Sudarshan Sumathipala <118509143+sumathipala-hes@users.noreply.github.com>
2025-03-07 15:34:33 +0530

    feat: Define data models for User, Task, Category, and related entities in Prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,28 +4,28 @@ const prisma = new PrismaClient();
 
 async function main() {
   // Create Sample1 records
-  const sample1A = await prisma.sample1.create({
-    data: {
-      sampleField: "Sample 1A",
-      sample2: {
-        create: [
-          { sampleField: "Sample 2A related to 1A" },
-          { sampleField: "Sample 2B related to 1A" },
-        ],
-      },
-    },
-  });
+  // const sample1A = await prisma.sample1.create({
+  //   data: {
+  //     sampleField: "Sample 1A",
+  //     sample2: {
+  //       create: [
+  //         { sampleField: "Sample 2A related to 1A" },
+  //         { sampleField: "Sample 2B related to 1A" },
+  //       ],
+  //     },
+  //   },
+  // });
 
-  const sample1B = await prisma.sample1.create({
-    data: {
-      sampleField: "Sample 1B",
-      sample2: {
-        create: [{ sampleField: "Sample 2C related to 1B" }],
-      },
-    },
-  });
+  // const sample1B = await prisma.sample1.create({
+  //   data: {
+  //     sampleField: "Sample 1B",
+  //     sample2: {
+  //       create: [{ sampleField: "Sample 2C related to 1B" }],
+  //     },
+  //   },
+  // });
 
-  console.log({ sample1A, sample1B });
+  // console.log({ sample1A, sample1B });
 }
 
 main()
