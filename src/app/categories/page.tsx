11d5d565ba3eadab0f332b7fd747efd8baa1e209
Sudarshan Sumathipala <118509143+sumathipala-hes@users.noreply.github.com>
2025-03-14 13:42:29 +0530

    remove comment

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -14,11 +14,6 @@ export default function CategoriesPage() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
         </div>
       </div>
-
-      {/* <DeleteCategoryDialog
-        open={isDeleteDialogOpen}
-        onOpenChange={setIsDeleteDialogOpen}
-      /> */}
     </div>
   );
 }
